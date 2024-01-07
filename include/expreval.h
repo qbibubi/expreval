ab8877ead691e5b797d81f9ce65457c9de9329c5
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2024-01-07 21:43:45 +0100

    update

--- include/expreval.h
+++ include/expreval.h
@@ -1,6 +1,14 @@
 #pragma once
 
 #include "debug.h"
-#include "stack.h"
+#include <algorithm>
+#include <cctype>
+#include <queue>
+#include <stack>
 
-namespace expreval {}; // namespace expreval
+namespace expreval {
+
+void shunting_yard(const std::string &expression);
+void remove_whitespace(std::string &expression);
+
+}; // namespace expreval
--- src/expreval.cpp
+++ src/expreval.cpp
@@ -1,1 +1,15 @@
 #include "expreval.h"
+#include <cctype>
+
+void expreval::shunting_yard(td::string &expression) {
+  std::stack<std::string> operators;
+  std::queue<std::string> postfix_expressions;
+
+  remove_whitespace(expression);
+}
+
+void expreval::remove_whitespace(std::string &expression) {
+  expression.erase(
+      std::remove_if(expression.begin(), expression.end(), ::isspace),
+      expression.end());
+}

a4602d48afd17ac3363772d741e2a6aab9c31432
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2024-01-08 16:30:39 +0100

    update: working on tokenization

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
 #include "include/expreval.h"
 
-int main() { return 0; }
+int main() {
+  std::string expression = "(12 + 3) * (3 - 2) - 1";
+  expreval::shunting_yard(expression);
+  return 0;
+}
--- src/expreval.cpp
+++ src/expreval.cpp
@@ -1,15 +1,35 @@
 #include "expreval.h"
-#include <cctype>
-
-void expreval::shunting_yard(td::string &expression) {
-  std::stack<std::string> operators;
-  std::queue<std::string> postfix_expressions;
-
-  remove_whitespace(expression);
-}
 
 void expreval::remove_whitespace(std::string &expression) {
   expression.erase(
       std::remove_if(expression.begin(), expression.end(), ::isspace),
       expression.end());
+}
+
+std::vector<char> expreval::tokenize(const std::string &expression) {
+  std::vector<char> tokens;
+  for (const auto &token : expression) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+void expreval::shunting_yard(std::string &expression) {
+  if (expression.empty() && expression.data() != nullptr) {
+    return;
+  }
+
+  std::stack<char> operators;
+  std::stack<char> output;
+  remove_whitespace(expression);
+
+  // tokenize
+  std::vector<char> tokens = tokenize(expression);
+  while (!tokens.empty()) {
+    for (const auto &token : tokens) {
+      if (std::isdigit(token)) {
+        output.push(token);
+      }
+    }
+  }
 }

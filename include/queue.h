3c8883629c21ef22a4c17e4558981fa75839052a
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2024-01-10 23:15:03 +0100

    update: adding my onw queue

--- include/queue.h
+++ include/queue.h
@@ -1,3 +1,31 @@
 #pragma once
 
-namespace queue {};
+#include <memory>
+#include <optional>
+
+namespace queue {
+
+template <typename T> class Queue {
+public:
+  Queue() {}
+  ~Queue() = default;
+
+  std::optional<T> front() const;
+  std::optional<T> back() const;
+  bool empty() const;
+  int size() const;
+  void push(const T &value);
+  void pop();
+
+private:
+  struct Node {
+    Node(const T &value) : data(value), next(nullptr) {}
+    T data;
+    std::unique_ptr<Node> next;
+  };
+
+  std::unique_ptr<T> m_front;
+  T *m_back;
+};
+
+}; // namespace queue
--- src/queue.cpp
+++ src/queue.cpp
@@ -1,1 +1,23 @@
 #include "queue.h"
+
+template <typename T> bool queue::Queue<T>::empty() const {
+  return this->m_front == nullptr;
+}
+
+template <typename T> int queue::Queue<T>::size() const {
+  //
+}
+
+template <typename T> void queue::Queue<T>::push(const T &value) {
+  auto new_node = std::make_unique<Node>(value);
+  if (!this->empty()) {
+    this->m_back->next = std::move(new_node);
+
+  } else {
+    this->m_front = this->m_back = new_node.get();
+    this->m_back = this->m_back->next.get();
+  }
+}
+
+template <typename T> std::optional<T> queue::Queue<T>::front() const {}
+template <typename T> std::optional<T> queue::Queue<T>::back() const {}
